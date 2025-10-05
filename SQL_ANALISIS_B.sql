5784bec878ab673130398a85d8fed2142051aee8
Jeffersonmz2 <Jeffersonmz2@users.noreply.github.com>
2025-10-05 13:46:54 -0500

    SQL PROBLEMA B

---
+++ SQL_ANALISIS_B.sql
@@ -0,0 +1,9 @@
+SELECT 
+    u.NOMBRE || ' ' || u.APELLIDO AS nombre_completo,
+    c.NOMBRE AS nombre_curso
+FROM 
+    CURSOS c
+    LEFT JOIN CALENDARIO_CURSOS cc ON c.ID_CURSO = cc.ID_CURSO
+    LEFT JOIN USUARIOS u ON cc.ID_PROFESOR = u.ID_USUARIO
+WHERE 
+    c.ID_FACULTAD = 2;
